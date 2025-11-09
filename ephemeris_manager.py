b548ded02028e2f90ef4d0bc00a459f1b28fa812
ilya-pauzner <ilya-pauzner@yandex.ru>
2025-11-09 17:19:26 +0200

    fix errors

--- ephemeris_manager.py
+++ ephemeris_manager.py
@@ -1,14 +1,10 @@
-from ftplib import FTP_TLS, FTP
-import ftplib
-import gzip
-import shutil
 import os
 from datetime import datetime, timezone
 import georinex
-import unlzw3
 import pandas as pd
 import numpy as np
 from gnss_lib_py.utils.ephemeris_downloader import load_ephemeris
+from astropy.time import Time
 
 
 class EphemerisManager():
@@ -41,9 +37,9 @@ class EphemerisManager():
     def load_data(self, timestamp, constellations=None):
         # TODO: pick constellations?
         data_list = []
-        files = load_ephemeris(file_type="rinex_nav", gps_millis=timestamp, constellations=constellations, download_directory="ephemeris_data")
+        files = load_ephemeris(file_type="rinex_nav", gps_millis=Time(timestamp).gps * 1000, constellations=constellations, download_directory="ephemeris_data")
         for file in files:
-            data_list.append(self.read_ephemeris(file))
+            data_list.append(self.read_ephemeris(file, constellations=constellations))
         if data_list:
             data = pd.concat(data_list, ignore_index=True)
         else:
@@ -54,7 +50,7 @@ class EphemerisManager():
         self.data = data
         
         
-    def read_ephemeris(self, decompressed_filename):
+    def read_ephemeris(self, decompressed_filename, constellations=None):
         if not self.leapseconds:
             self.leapseconds = EphemerisManager.load_leapseconds(
                 decompressed_filename)
@@ -73,40 +69,6 @@ class EphemerisManager():
         data.rename(columns={'M0': 'M_0', 'Eccentricity': 'e', 'Toe': 't_oe', 'DeltaN': 'deltaN', 'Cuc': 'C_uc', 'Cus': 'C_us',
                              'Cic': 'C_ic', 'Crc': 'C_rc', 'Cis': 'C_is', 'Crs': 'C_rs', 'Io': 'i_0', 'Omega0': 'Omega_0'}, inplace=True)
         return data
-
-
-    def get_ephemeris_dataframe(self, fileinfo, constellations=None):
-        filepath = fileinfo['filepath']
-        url = fileinfo['url']
-        directory = os.path.split(filepath)[0]
-        filename = os.path.split(filepath)[1]
-        if url == 'igs.bkg.bund.de':
-            dest_filepath = os.path.join(self.data_directory, 'igs', filename)
-        else:
-            dest_filepath = os.path.join(self.data_directory, 'nasa', filename)
-        decompressed_filename = os.path.splitext(dest_filepath)[0]
-        if not os.path.isfile(decompressed_filename):
-            if url == 'gdc.cddis.eosdis.nasa.gov':
-                secure = True
-            else:
-                secure = False
-            try:
-                self.retrieve_file(url, directory, filename,
-                                   dest_filepath, secure)
-                self.decompress_file(dest_filepath)
-            except ftplib.error_perm:
-                print('ftp error')
-                return pd.DataFrame()
-        return read_ephemeris(self, decompressed_filename)
-
-    @staticmethod
-    def get_filetype(timestamp):
-        # IGS switched from .Z to .gz compression format on December 1st, 2020
-        if timestamp >= datetime(2020, 12, 1, 0, 0, 0, tzinfo=timezone.utc):
-            extension = '.gz'
-        else:
-            extension = '.Z'
-        return extension
 
     @staticmethod
     def load_leapseconds(filename):
@@ -130,84 +92,6 @@ class EphemerisManager():
     @staticmethod
     def calculate_toc(timestamp):
         pass
-
-    def retrieve_file(self, url, directory, filename, dest_filepath, secure=False):
-        print('Retrieving ' + directory + '/' + filename + ' from ' + url)
-        ftp = self.connect(url, secure)
-        src_filepath = directory + '/' + filename
-        try:
-            with open(dest_filepath, 'wb') as handle:
-                ftp.retrbinary(
-                    'RETR ' + src_filepath, handle.write)
-        except ftplib.error_perm as err:
-            print('Failed to retrieve ' + src_filepath + ' from ' + url)
-            print(err)
-            os.remove(dest_filepath)
-            raise ftplib.error_perm
-
-    def decompress_file(self, filepath):
-        extension = os.path.splitext(filepath)[1]
-        decompressed_path = os.path.splitext(filepath)[0]
-        if extension == '.gz':
-            with gzip.open(filepath, 'rb') as f_in:
-                with open(decompressed_path, 'wb') as f_out:
-                    shutil.copyfileobj(f_in, f_out)
-        elif extension == '.Z':
-            with open(filepath, 'rb') as f_in:
-                with open(decompressed_path, 'wb') as f_out:
-                    f_out.write(unlzw3.unlzw(f_in.read()))
-        os.remove(filepath)
-
-    def connect(self, url, secure):
-        if secure:
-            ftp = FTP_TLS(url)
-            ftp.login()
-            ftp.prot_p()
-        else:
-            ftp = FTP(url)
-            ftp.login()
-        return ftp
-
-    def listdir(self, url, directory, secure):
-        ftp = self.connect(url, secure)
-        dirlist = ftp.nlst(directory)
-        dirlist = [x for x in dirlist]
-        print(dirlist)
-
-    @staticmethod
-    def get_filepaths(timestamp):
-        timetuple = timestamp.timetuple()
-        extension = EphemerisManager.get_filetype(timestamp)
-        filepaths = {}
-
-        directory = 'gnss/data/daily/' + str(timetuple.tm_year) + '/brdc/'
-        filename = 'BRDC00IGS_R_' + \
-            str(timetuple.tm_year) + \
-            str(timetuple.tm_yday).zfill(3) + '0000_01D_MN.rnx.gz'
-        filepaths['nasa_daily_combined'] = {
-            'filepath': directory + filename, 'url': 'gdc.cddis.eosdis.nasa.gov'}
-
-        filename = 'brdc' + str(timetuple.tm_yday).zfill(3) + \
-            '0.' + str(timetuple.tm_year)[-2:] + 'n' + extension
-        filepaths['nasa_daily_gps'] = {
-            'filepath': directory + filename, 'url': 'gdc.cddis.eosdis.nasa.gov'}
-
-        filename = 'brdc' + str(timetuple.tm_yday).zfill(3) + \
-            '0.' + str(timetuple.tm_year)[-2:] + 'g' + extension
-        filepaths['nasa_daily_glonass'] = {
-            'filepath': directory + filename, 'url': 'gdc.cddis.eosdis.nasa.gov'}
-
-        directory = '/IGS/BRDC/' + \
-            str(timetuple.tm_year) + '/' + \
-            str(timetuple.tm_yday).zfill(3) + '/'
-        filename = 'BRDC00WRD_S_' + \
-            str(timetuple.tm_year) + \
-            str(timetuple.tm_yday).zfill(3) + '0000_01D_MN.rnx.gz'
-        filepaths['bkg_daily_combined'] = {
-            'filepath': directory + filename, 'url': 'igs.bkg.bund.de'}
-
-        return filepaths
-
 
 if __name__ == '__main__':
     repo = EphemerisManager()
